6b856b38ab7729a292d41f8f81e5dad6eb127c88
tommyjakobsen <tommy.jakobsen@outlook.com>
2018-12-14 14:38:14 +0100

    working index with translate

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ if(getenv('SQS_AWS_ACCESS_KEY_ID') !== false)
     {
         $SQS_AWS_ACCESS_KEY_ID=getenv('SQS_AWS_ACCESS_KEY_ID');
        //Debug
-	//echo "Found Access Key: $SQS_AWS_ACCESS_KEY_ID<hr>";
-	putenv("AWS_ACCESS_KEY_ID=$SQS_AWS_ACCESS_KEY_ID");
+        //echo "Found Access Key: $SQS_AWS_ACCESS_KEY_ID<hr>";
+        putenv("AWS_ACCESS_KEY_ID=$SQS_AWS_ACCESS_KEY_ID");
      }else{
         echo "<font color=red><b>! \$SQS_AWS_ACCESS_KEY_ID variable not set...<br>Add secrets to application...</font>";
         echo "</body>";
@@ -16,8 +16,8 @@ if(getenv('SQS_AWS_SECRET_ACCESS_KEY') !== false)
     {
         $SQS_AWS_SECRET_ACCESS_KEY=getenv('SQS_AWS_SECRET_ACCESS_KEY');
         //DEBUG
-	//echo "Found aiKey at: $SQS_AWS_SECRET_ACCESS_KEY<hr>";
-	putenv("AWS_SECRET_ACCESS_KEY=$SQS_AWS_SECRET_ACCESS_KEY");
+        //echo "Found aiKey at: $SQS_AWS_SECRET_ACCESS_KEY<hr>";
+        putenv("AWS_SECRET_ACCESS_KEY=$SQS_AWS_SECRET_ACCESS_KEY");
      }else{
         echo "<font color=red><b>! \$SQS_AWS_SECRET_ACCESS_KEY variable not set...<br>Check bindings in openshift...</font>";
         echo "</body>";
@@ -30,7 +30,7 @@ use Aws\Translate\TranslateClient;
 use Aws\Translate\Exception;
 
 $client = new Aws\Translate\TranslateClient([
-      'region' => 'us-east-1',
+    'region' => 'us-east-1',
     'version' => 'latest',
 
 ]);
@@ -39,7 +39,13 @@ $currentLanguage = 'es';
 
 $targetLanguage= 'en';
 
-$textToTranslate = 'El AWS SDK for PHP versión 3 permite a los desarrolladores de PHP utilizar Amazon Web Services en su código PHP y crear aplicaciones y software robustos utilizando servicios como Amazon S3, Amazon DynamoDB, Amazon Glacier, etc. Puede empezar rápidamente instalando el SDK mediante Composer (solicitando elpaquete aws/aws-sdk-php) o descargando el archivo aws.zip o aws.phar independiente';
+$textToTranslate = '¡Ahora estoy tan enojado con esta mierda!
+¡Mi red pierde conexión todo el tiempo!
+
+¡Mis genes españoles están gritando de furia!
+¡AYUDAME AHORA';
+
+
 
 try {
     $result = $client->translateText([
@@ -47,9 +53,18 @@ try {
         'TargetLanguageCode' => $targetLanguage,
         'Text' => $textToTranslate,
     ]);
-    var_dump($result);
+//DEBUG
+// var_dump($result);
 }catch (AwsException $e) {
     // output error message if fails
-    echo $e->getMessage();
-    echo "\n";
+    echo preg_replace('/\n/', '<br>'. $e->getMessage());
+    echo "";
+    exit(1);
 }
+
+$result = $result->toArray();
+echo "$textToTranslate<hr>":
+echo $result["TranslatedText"]."<hr>";
+
+
+?>
